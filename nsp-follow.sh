b24f3b9612909476af7c1852de6d57b10affde60
NSproGAMERSYT <87557923+RHYTHMHACKER@users.noreply.github.com>
2022-08-19 10:32:10 +0530

    Update nsp-follow.sh

--- nsp-follow.sh
+++ nsp-follow.sh
@@ -21,7 +21,6 @@ var2=$(echo $var | grep -o 'csrftoken=.*' | cut -d ';' -f1 | cut -d '=' -f2)
 ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 
 banner() {
-printf " \n"
 
 ╭━╮╱╭┳━━━╮╱╱╱╱╱╱╭╮╱╱╭┳━━━━╮
 ┃┃╰╮┃┃╭━╮┃╱╱╱╱╱╱┃╰╮╭╯┃╭╮╭╮┃
@@ -30,10 +29,7 @@ printf " \n"
 ┃┃╱┃┃┃╰━╯┃╰╯┃┃┃╰╯┃┃┃╱╱╱┃┃
 ╰╯╱╰━┻━━━┫╭━┻╯╰━━╯╰╯╱╱╱╰╯
 ╱╱╱╱╱╱╱╱╱┃┃
-╱╱╱╱╱╱╱╱╱╰╯                                                                    \n"
-printf "\n"
-
-
+╱╱╱╱╱╱╱╱╱╰╯              }                                                      \n"
 printf "      \e[1;31m[\e[0m\e[1;77mv1.0\e[0m\e[1;31m]\e[0m\e[1;77m github.com/RHYTHMHACKER/nsp-follow\e[0m\n"
 }
 
